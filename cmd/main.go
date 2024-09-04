14387db3e01e60be44ba8304c0e77736acd776eb
alexmllo <amitjansllorach1@gmail.com>
2024-09-04 23:48:49 +0200

    push

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,26 @@ import (
 	"log"
 
 	"github.com/alexmllo/ecommerce/cmd/api"
+	"github.com/alexmllo/ecommerce/config"
+	"github.com/alexmllo/ecommerce/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPasword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
